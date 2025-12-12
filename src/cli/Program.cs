0a4e9c1912d5c8d9cef696bf14b42541dfe51975
rschleitzer <ralf@scaly.io>
2025-12-12 14:15:33 +0100

    Add NuGet packaging for library and CLI tool

--- src/cli/Program.cs
+++ src/cli/Program.cs
@@ -1,4 +1,12 @@
 using Dazzle.Cli;
+using System.Reflection;
+
+// Add bundled SGML catalog to args
+var assemblyDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
+var catalogPath = Path.Combine(assemblyDir, "sgml", "catalog");
+
+var newArgs = new List<string> { "-c", catalogPath };
+newArgs.AddRange(args);
 
 var app = new DazzleApp();
-return app.run(args);
+return app.run(newArgs.ToArray());
